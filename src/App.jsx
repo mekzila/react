3fcecb3140a646144f4a732f8b408cdf1d196cf1
unknown <you@example.com>
2024-03-14 10:16:07 +0100

    4th

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,28 @@
-
 import Header from './Component/Header';
 import Footer from './Component/Footer';
 import Contact from './Component/Contact';
 import Home from './Component/Home';
 import Services from './Component/Services';
 import Tracking from './Component/Tracking';
-import React,{useEffect} from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import React, { useEffect } from 'react';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 
 function App() {
   useEffect(() => {
-    AOS.init({duration: 2000 }); // Global settings for AOS (optional)
-  }, []) // Ensure this useEffect runs only once after component mount
+    AOS.init({
+      // Global settings for AOS (optional)
+      duration: 3000
+    });
+  }, []); // Ensure this useEffect runs only once after component mount
   return (
     <>
     <Header/>
     <Router>
       <Routes>
         <Route exact path="/" element={<Home/>}/>
-        <Route exact path="/service" element={<Service/>}/>
+        <Route exact path="/service" element={<Services/>}/>
         <Route exact path="/tracking" element={<Tracking/>}/>
         <Route exact path="/contact" element={<Contact/>}/>
       </Routes>
